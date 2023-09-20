7abb01b67d233f7ca574fa74a958bba54ff76dbe
Edilson Ferreira <edilsonferreira.2017@outlook.com.br>
2023-09-19 22:03:57 -0300

    Commit

--- Projeto-Checkout/src/components/Login.tsx
+++ Projeto-Checkout/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import {Bitcoin, BlockCode, BlockDate, BlockHeader, BlockInput, BlockRodape, BlockTitleRodape, BlockvalueTitle, BlocokIcons, BookHeader, ButtonRodaoe, Container, Content, ContentHeader, ContentInput, ContentPayment, ContentTitle, CreditCart, HeaderTitle, IconBitcoin, IconCreditCart, IconPaypal, ImgRodape, InputCode, InputDate, InputName, InputNumber, Label, LabelName, LogoHeader, Number, Paypal, RodapeLabel, TitleBitcoin, TitleChechkout, TitleCreditCart, TitlePayment, TitlePaypal, TitleRodapeCinza, TitleRodapeorange, ValueTitle } from "./styles";
+import {Bitcoin, Block, BlockBottun, BlockCode, BlockDate, BlockHeader, BlockInput, BlockName, BlockNumber, BlockRodape, BlockTitleRodape, BlockvalueTitle, BlocokIcons, BookHeader, ButtonRodaoe, Container, Content, ContentHeader, ContentInput, ContentPayment, ContentTitle, CreditCart, HeaderTitle, IconBitcoin, IconCreditCart, IconPaypal, ImgRodape, InputCode, InputDate, InputName, InputNumber, Label, LabelName, LogoHeader, Number, Paypal, RodapeLabel, TitleBitcoin, TitleChechkout, TitleCreditCart, TitlePayment, TitlePaypal, TitleRodapeCinza, TitleRodapeorange, ValueTitle } from "./styles";
 import Logo from "../assets//32c90c58b9248b11396155be8de569cd.png"
 import LogoCredit from "../assets/cartao-de-credito.png"
 import LogoPaypal from "../assets/logotipo.png"
@@ -12,6 +12,25 @@ export function Login(){
   
 const [newNumber,setNumber] = useState('')
 
+const [newName, setName] = useState('')
+
+const [newCode, setCode] = useState('')
+
+const [newDate,setDate] = useState('')
+
+function HanldeDate(e: ChangeEvent<HTMLInputElement>){
+  setDate(e.target.value)
+}
+
+function HandleCode(e: ChangeEvent<HTMLInputElement>){
+  setCode(e.target.value)
+}
+
+function HandleChange(e: ChangeEvent<HTMLInputElement>){
+  setName(e.target.value)
+}
+
+
 function HandleNumber(e: ChangeEvent<HTMLInputElement>){
   setNumber(e.target.value)
 }
@@ -24,6 +43,33 @@ function HandleSubmit(e: FormEvent){
     console.log(error)
   }finally {
     setNumber('')
+  }
+
+  try {
+    e.preventDefault()
+    console.log(newName)
+  } catch (error) {
+    console.log(error)
+  }finally{
+    setName('')
+  }
+
+  try {
+    e.preventDefault()
+    console.log(newCode)
+  } catch (error) {
+    console.log(error)
+  }finally{
+    setCode('')
+  }
+
+  try {
+    e.preventDefault()
+    console.log(newDate)
+  } catch (error) {
+    console.log(error)
+  }finally{
+  setDate('')
   }
 }
   return(
@@ -74,62 +120,112 @@ function HandleSubmit(e: FormEvent){
             </BlockvalueTitle>
       
           </BookHeader>
-      </ContentHeader>
+        </ContentHeader>
 
-      <ContentPayment>
-        <TitlePayment>
-          My payment method
-        </TitlePayment>
-      </ContentPayment>
+        <ContentPayment>
+          <TitlePayment>
+            My payment method
+          </TitlePayment>
+        </ContentPayment>
 
-      <BlocokIcons>
-        <CreditCart>
-          <IconCreditCart src={LogoCredit} alt=""/>
-          <TitleCreditCart>
-            Credit Cart
-          </TitleCreditCart>
-        </CreditCart>
+        <BlocokIcons>
+          <CreditCart>
+            <IconCreditCart 
+              src={LogoCredit} 
+              alt=""
+            />
+            <TitleCreditCart>
+              Credit Cart
+            </TitleCreditCart>
+          </CreditCart>
 
-        <Paypal>
-          <IconPaypal src={LogoPaypal} alt="" />
-          <TitlePaypal>
-            Paypal
-          </TitlePaypal>
-        </Paypal>
+          <Paypal>
+            <IconPaypal 
+              src={LogoPaypal} 
+              alt="" 
+            />
+            <TitlePaypal>
+              Paypal
+            </TitlePaypal>
+          </Paypal>
 
-        <Bitcoin>
-          <IconBitcoin src={LogoBitcoin} alt="" />
-          <TitleBitcoin>
-            Bitcoin
-          </TitleBitcoin>
-        </Bitcoin>
-      </BlocokIcons>
+          <Bitcoin>
+            <IconBitcoin 
+              src={LogoBitcoin} 
+              alt="" 
+            />
+            <TitleBitcoin>
+              Bitcoin
+            </TitleBitcoin>
+          </Bitcoin>
+        </BlocokIcons>
 
         <ContentInput onSubmit={HandleSubmit}>
-          <Label htmlFor="type">Card Number   
-          </Label>
-          <InputNumber
-           type="numero" 
-           value={newNumber}
-           onChange={HandleNumber}
-           />
-          <LabelName htmlFor="text">Name on Card</LabelName>
-          <InputName type="text" />
+          
+          <Block>
+            <BlockNumber>
+              <Label 
+                htmlFor="type">
+                Card Number   
+              </Label>
+              <InputNumber
+                type="numero" 
+                value={newNumber}
+                onChange={HandleNumber}
+              />
+            </BlockNumber>
+              <BlockName>
+                <LabelName 
+                  htmlFor="text">
+                  Name on Card
+                </LabelName>
+                <InputName 
+                  type="text" 
+                  value={newName}
+                  onChange={HandleChange}
+                />
+            </BlockName>
+          </Block>
+
           <BlockInput>
             <BlockCode>
-              <RodapeLabel htmlFor="text">CVV CODE</RodapeLabel>
-              <InputCode type="password" />
+              <RodapeLabel 
+                htmlFor="text">
+                CVV CODE
+                </RodapeLabel>
+              <InputCode 
+                type="password" 
+                value={newCode}
+                onChange={HandleCode}
+              />
             </BlockCode>
             <BlockDate>
-              <RodapeLabel htmlFor="">Expiration date</RodapeLabel>
-              <InputDate type="text" placeholder="MM/YY"/>
+              <RodapeLabel 
+                htmlFor="">
+                Expiration date
+              </RodapeLabel>
+              <InputDate 
+                type="text" 
+                placeholder="MM/YY"
+                value={newDate}
+                onChange={HanldeDate}
+              />
             </BlockDate>
           </BlockInput>
+          <BlockBottun>
+            <ButtonRodaoe 
+              type="submit">
+              Submit Orden
+            </ButtonRodaoe>
+          </BlockBottun>
         </ContentInput>
 
           <BlockRodape>
             <BlockTitleRodape>
-              <ImgRodape src={LogoSeta} alt=""/>
+              <ImgRodape 
+                src={LogoSeta} 
+                alt=""
+              />
               <TitleRodapeCinza>
                 l have read & agree to
               </TitleRodapeCinza> 
@@ -137,9 +233,6 @@ function HandleSubmit(e: FormEvent){
                 Terms & Conditions
               </TitleRodapeorange>
             </BlockTitleRodape>
-            <ButtonRodaoe type="submit">
-              Submit Orden
-            </ButtonRodaoe>
           </BlockRodape>
       </Container>
     </Content>
--- Projeto-Checkout/src/components/styles.ts
+++ Projeto-Checkout/src/components/styles.ts
@@ -2,93 +2,80 @@ import styled from "styled-components";
 
 export const Content = styled.div`
   width: 100%;
-  height: 150vh;
+  height: 100vh;
   background: #40EBF5;
 
-
 `
-
 export const Container = styled.div`
   background: white;
+  width: 60%;
   height: 100%;
-  width: 88.2%;
-  margin-left: 6%;
+  margin-left: 19%;
   box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-  
 `
 
 export const ContentHeader = styled.div`
   display: flex;
   width: 100%;
-  height: 70px;
+  height: 4%;
   background:#4C4C6D;
-  gap: 710px;
 
 `
 export const BlockHeader = styled.div`
   display: flex;
-  width: 20%;
+  margin-left: 2%;
+  width: 5%;
+  gap: 5px;
 `
-
 export const LogoHeader = styled.img`
   width: 93px;
-  margin-left: 10%;
-  
 `
 export const ContentTitle = styled.div`
   display: flex;
-  margin-top: 8.5%;
-  margin-left: 10%;
+  margin-top: 20%;
+  margin-left: 2%;
+  width: 300%;
 `
 export const Number = styled.h1`
-  font-size: 15px;
+  font-size: 11px;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   color: white;
 `
 export const HeaderTitle = styled.p`
-  font-size: 15px;
+  font-size: 11px;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   color: white;
   font-weight: 100;
   width: 108px;
-
 `
-
 export const BookHeader = styled.div`
   display: flex;
   margin-top: 1.2%;
-  gap: 60px;
+  margin-left: 65%;
+  gap: 20px;
 `
-
 export const TitleChechkout = styled.p`
-  font-size: 15px;
+  font-size: 11px;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   color: white;
-  margin-top: 3%;
 `
-
 export const BlockvalueTitle = styled.div`
   border: 1px solid white;
   border-radius: 25px;
-  height: 56%;
-  width: 120px;
-  
+  height: 53%;
+  width: 88px;
 `
-
 export const ValueTitle = styled.p`
-  font-size: 15px;
+  font-size: 10px;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   color: white;
   font-weight: 100;
   margin-left: 12%;
-  margin-top: 5%;
 `
-
 export const ContentPayment = styled.div`
   display: block;
   text-align: center;
 `
-
 export const TitlePayment = styled.p`
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   font-size: 34px;
@@ -97,24 +84,22 @@ export const TitlePayment = styled.p`
   color: #4C4C6D;
   margin-top: 7%;
 `
-
 export const BlocokIcons = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 12px;
+  gap:20px;
   margin-top: 3%;
 `
-
 export const CreditCart = styled.div`
-  width: 15%;
-  height: 200px;
+  width: 20%;
+  height: 170px;
   background: #7bcfd4;
 `
 export const IconCreditCart = styled.img`
-  margin-left: 21.5%;
+  margin-left: 28%;
   margin-top: 15%;
-  width: 100px;
+  width: 60px;
 `
 export const TitleCreditCart = styled.p`
   text-align: center;
@@ -124,19 +109,16 @@ export const TitleCreditCart = styled.p`
   color: #4C4C6D;
   letter-spacing: 0.5px;
   margin-top: 15%;
-
 `
-
-
 export const Paypal = styled.div`
-  width: 15%;
-  height: 200px;
+  width: 20%;
+  height: 170px;
   border: solid 2px #e8ebeb;
 `
 export const IconPaypal = styled.img`
-  margin-left: 22%;
+  margin-left: 34%;
   margin-top: 15%;
-  width: 100px;
+  width: 60px;
 `
 export const TitlePaypal = styled.p`
   text-align: center;
@@ -147,17 +129,15 @@ export const TitlePaypal = styled.p`
   letter-spacing: 0.5px;
   margin-top: 15%;
 `
-
-
 export const Bitcoin = styled.div`
-  width: 15%;
-  height: 200px;
+  width: 20%;
+  height: 170px;
   border: solid 2px #e8ebeb;
 `
 export const IconBitcoin = styled.img`
-  margin-left: 22%;
+  margin-left: 30%;
   margin-top: 15%;
-  width: 100px;
+  width: 60px;
 `
 export const TitleBitcoin = styled.p`
   text-align: center;
@@ -168,17 +148,22 @@ export const TitleBitcoin = styled.p`
   letter-spacing: 0.5px;
   margin-top: 15%;
 `
-
 export const ContentInput = styled.form`
   display: flex;
   flex-direction: column;
-  margin-left: 26.3%;
-  width: 47.1%;
-  height: 28%;
+  margin-left: 17.2%;
+  width: 65%;
+  height: 38%;
   border: solid 2px #e8ebeb;
   margin-top: 3%;
-  padding-top: 4%;
-  gap: 10px;
+`
+export const Block = styled.div`
+  margin-top: 5.5%;
+`
+export const BlockNumber = styled.div`
+`
+export const BlockName = styled.div`
+  padding-top: 5%;
 `
 export const BlockInput = styled.div`
   display: flex;
@@ -196,8 +181,8 @@ export const BlockDate = styled.div`
   display: flex;
   flex-direction: column;
   gap: 9px;
+  width: 29.2%;
 `
-
 export const InputNumber = styled.input`
   margin-left: 11%;
   width: 75%;
@@ -205,6 +190,7 @@ export const InputNumber = styled.input`
   border-radius:4px;
   border:1px solid gray;
   padding-left: 2.5%;
+  margin-top: 1%;
 `
 export const InputName = styled.input`
   margin-left: 11%;
@@ -213,6 +199,7 @@ export const InputName = styled.input`
   border-radius:4px;
   border:1px solid gray;
   padding-left: 2.5%;
+  margin-top: 1%;
 `
 export const InputCode = styled.input`
   width: 89%;
@@ -222,8 +209,8 @@ export const InputCode = styled.input`
   padding-left: 5%;
 `
 export const InputDate = styled.input`
-  width: 147%;
-  height: 33px;
+  width: 138%;
+  height: 25%;
   border-radius:4px;
   border:1px solid gray;
   padding-left: 5%;
@@ -236,7 +223,6 @@ export const Label = styled.label`
 `
 export const LabelName = styled.label`
   margin-left: 11%;
-  margin-top: 5%;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   font-size: 14px;
   color: gray;
@@ -246,12 +232,10 @@ export const RodapeLabel = styled.label`
   font-size: 14px;
   color: gray;
 `
-
 export const BlockRodape = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 5%;
+  margin-left: 22%;
+  margin-top: 4%;
   gap: 10px;
 `
 export const BlockTitleRodape = styled.div`
@@ -259,27 +243,29 @@ export const BlockTitleRodape = styled.div`
   gap: 5px;
 `
 export const ImgRodape = styled.img`
-  width: 15px;
+  width: 13px;
 `
 export const TitleRodapeCinza = styled.p`
-  font-size: 16px;
+  font-size: 11px;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   color: gray;
 `
-
 export const TitleRodapeorange = styled.p`
-  font-size: 16px;
+  font-size: 11px;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   color: #fa6b05;
 `
+export const BlockBottun = styled.div`
 
+`
 export const ButtonRodaoe = styled.button`
-  font-size: 16px;
+  font-size: 12px;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   color: white;
   background: #fa6b05;
   border:none;
   border-radius: 30px;
-  width: 13%;
-  height: 50px;
+  width: 25%;
+  height: 24px;
+  margin-left: 70%;
 `
